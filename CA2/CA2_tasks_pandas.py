6e018a380aa6e3ae300ebf50143f4f9489bf6d5e
Joao Sanchez <jvmsanchez0@gmail.com>
2025-11-30 14:32:41 +0000

    update

--- CA2/CA2_tasks_pandas.py
+++ CA2/CA2_tasks_pandas.py
@@ -1,6 +1,62 @@
 import pandas as pd
 
-path = 'C:\\Python Cursos\\My Projects\\StudentGrades.csv'
-df = pd.read_csv(path)
+def my_df():
+    path = 'C:\\Python Cursos\\My Projects\\StudentGrades.csv'
+    df = pd.read_csv(path)
+    # print("csv File:")
+    # print(df)
+    # print("")
 
-print(df)
+
+    df = df.rename(columns={"Module 1": "Chemistry", "Module 2": "Physics", "Module 3": "Biology"})
+    # print(f"Columns after renaming: {df.columns.tolist()}")
+    # print("")
+
+
+    cols = list(df.columns[:3])
+    for col in cols:
+        df[col] = df[col].astype(str).str[-2:].astype(int)
+
+    # print(f"The columns {cols} have been changed.")
+    # print(df)
+    # print("")
+
+
+    df[["Name", "Surname"]] = df["Student Name"].str.split(" ", n=1, expand=True)
+    df = df.drop(columns=["Student Name"])
+    # print("The column 'Student Name' has been split into 'Name' and 'Surname'")
+    # print(df)
+    # print("")
+
+
+    df["Total"] = (df["Chemistry"] * 0.5) + (df["Physics"] * 0.25) + (df["Biology"] * 0.25)
+    print(df)
+    return df
+
+
+
+
+def df_analyser(dataframe):
+    df_chemistry = dataframe[["Chemistry", "Name", "Surname"]]
+    df_physics = dataframe[["Physics", "Name", "Surname"]]
+    df_biology = dataframe[["Biology", "Name", "Surname"]]
+
+    df_chemistry = df_chemistry.sort_values(by="Surname")
+    df_physics = df_physics.sort_values(by="Surname")
+    df_biology = df_biology.sort_values(by="Surname")
+
+    df_chemistry.to_csv('C:\\Python Cursos\\My Projects\\df_chemistry.csv')
+    df_physics.to_csv('C:\\Python Cursos\\My Projects\\df_physics.csv')
+    df_biology.to_csv('C:\\Python Cursos\\My Projects\\df_biology.csv')
+    
+    print(df_chemistry)
+    print("")
+    print(df_physics)
+    print("")
+    print(df_biology)
+
+
+
+
+
+df_analyser(my_df())
